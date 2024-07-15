9ddb95246896aae2c4eeb061b74b48ab38713ac3
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-15 09:48:33 +0200

    Basic tests working

--- src/error.rs
+++ src/error.rs
@@ -18,6 +18,31 @@ pub enum MungeError {
     CredRewound,
     CredReplayed,
     CredUnauthorized,
+    Success,
 }
 
-pub enum Error {}
+impl MungeError {
+    pub fn to_u32(self) -> u32 {
+        match self {
+            MungeError::Snafu => crate::munge_err_EMUNGE_SNAFU,
+            MungeError::BadArg => crate::munge_err_EMUNGE_BAD_ARG,
+            MungeError::BadLength => crate::munge_err_EMUNGE_BAD_LENGTH,
+            MungeError::Overflow => crate::munge_err_EMUNGE_OVERFLOW,
+            MungeError::NoMemory => crate::munge_err_EMUNGE_NO_MEMORY,
+            MungeError::Socket => crate::munge_err_EMUNGE_SOCKET,
+            MungeError::Timeout => crate::munge_err_EMUNGE_TIMEOUT,
+            MungeError::BadCred => crate::munge_err_EMUNGE_BAD_CRED,
+            MungeError::BadVersion => crate::munge_err_EMUNGE_BAD_VERSION,
+            MungeError::BadCipher => crate::munge_err_EMUNGE_BAD_CIPHER,
+            MungeError::BadMac => crate::munge_err_EMUNGE_BAD_MAC,
+            MungeError::BadZip => crate::munge_err_EMUNGE_BAD_ZIP,
+            MungeError::BadRealm => crate::munge_err_EMUNGE_BAD_REALM,
+            MungeError::CredInvalid => crate::munge_err_EMUNGE_CRED_INVALID,
+            MungeError::CredExpired => crate::munge_err_EMUNGE_CRED_EXPIRED,
+            MungeError::CredRewound => crate::munge_err_EMUNGE_CRED_REWOUND,
+            MungeError::CredReplayed => crate::munge_err_EMUNGE_CRED_REPLAYED,
+            MungeError::CredUnauthorized => crate::munge_err_EMUNGE_CRED_UNAUTHORIZED,
+            MungeError::Success => crate::munge_err_EMUNGE_SUCCESS,
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -14,34 +14,72 @@ pub mod mungeOption;
 ///     credential
 ///     munge_ctx_t
 ///     buffer, length <- payload
-pub fn encode(payload: Option<&'_ [u8]>) -> Result<String, error::Error> {
+///
+/// C prototype: `munge_err_t munge_encode(char **cred, munge_ctx_t ctx, const void *buf, int len);`
+pub fn encode(payload: Option<&'_ [u8]>) -> Result<String, error::MungeError> {
     todo!()
 }
 
 // TODO: Implement encode decode test
 #[cfg(test)]
 mod tests {
-    use libc::c_char;
 
     use super::*;
-    use std::{env, ptr};
+    use std::ptr;
 
     #[test]
     fn roundtrip_encode_decode() {
-        let cred: *mut *mut c_char = ptr::null_mut();
-        let mut ctx = ctx::MungeCtx::default();
-        ctx.set_socket("/usr/local/var/run/munge/munge.socket.2".into());
+        let mut cred: *mut *mut ::std::os::raw::c_char = Box::into_raw(Box::new(ptr::null_mut()));
+        let ctx = unsafe { munge_ctx_create() };
+        if ctx.is_null() {
+            panic!("Failed to create munge context!");
+        }
+        let mut err: u32 = 42;
+        unsafe {
+            munge_ctx_set(
+                ctx,
+                munge_opt_MUNGE_OPT_SOCKET as i32,
+                "/usr/local/var/run/munge/munge.socket.2",
+            );
+            err = munge_encode(cred, ptr::null_mut(), ptr::null(), 0);
+        }
+        assert_eq!(err, munge_err_EMUNGE_SUCCESS);
 
-        let result = unsafe {
-            munge_encode(
-                cred,
-                ctx.ctx() as *const _ as *mut crate::munge_ctx,
-                ptr::null(),
-                0,
-            )
-        };
+        let cred_value: *const ::std::os::raw::c_char = unsafe { *cred };
 
-        println!("Encode: {:?}, Return: {:?}", cred, result);
-        println!("bindings.rs: {}", env!("OUT_DIR"));
+        let mut uid: u32 = 42;
+        let mut gid: u32 = 69;
+        let mut decode_err: u32 = 42;
+        unsafe {
+            decode_err = munge_decode(
+                cred_value,
+                ptr::null_mut(),
+                ptr::null_mut(),
+                ptr::null_mut(),
+                &mut uid,
+                &mut gid,
+            );
+        }
+        println!("UID: {}, \tGID: {}", uid, gid);
+        assert_eq!(decode_err, munge_err_EMUNGE_SUCCESS);
+    }
+
+    #[test]
+    fn encode() {
+        let mut cred: *mut *mut ::std::os::raw::c_char = Box::into_raw(Box::new(ptr::null_mut()));
+        let ctx = unsafe { munge_ctx_create() };
+        if ctx.is_null() {
+            panic!("Failed to create munge context!");
+        }
+        let mut err: u32 = 42;
+        unsafe {
+            munge_ctx_set(
+                ctx,
+                munge_opt_MUNGE_OPT_SOCKET as i32,
+                "/usr/local/var/run/munge/munge.socket.2",
+            );
+            err = munge_encode(cred, ptr::null_mut(), ptr::null(), 0);
+        }
+        assert_eq!(err, munge_err_EMUNGE_SUCCESS);
     }
 }
