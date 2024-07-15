28312a4a8f00b2342c049598931eafd1004d6fbc
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-15 13:15:18 +0200

    Added wrapper for munge_ctx

--- src/error.rs
+++ src/error.rs
@@ -45,4 +45,30 @@ impl MungeError {
             MungeError::Success => crate::munge_err_EMUNGE_SUCCESS,
         }
     }
+
+    pub fn from_u32(err: u32) -> MungeError {
+        match err {
+            crate::munge_err_EMUNGE_SNAFU => MungeError::Snafu,
+            crate::munge_err_EMUNGE_BAD_ARG => MungeError::BadArg,
+            crate::munge_err_EMUNGE_BAD_LENGTH => MungeError::BadLength,
+            crate::munge_err_EMUNGE_OVERFLOW => MungeError::Overflow,
+            crate::munge_err_EMUNGE_NO_MEMORY => MungeError::NoMemory,
+            crate::munge_err_EMUNGE_SOCKET => MungeError::Socket,
+            crate::munge_err_EMUNGE_TIMEOUT => MungeError::Timeout,
+            crate::munge_err_EMUNGE_BAD_CRED => MungeError::BadCred,
+            crate::munge_err_EMUNGE_BAD_VERSION => MungeError::BadVersion,
+            crate::munge_err_EMUNGE_BAD_CIPHER => MungeError::BadCipher,
+            crate::munge_err_EMUNGE_BAD_MAC => MungeError::BadMac,
+            crate::munge_err_EMUNGE_BAD_ZIP => MungeError::BadZip,
+            crate::munge_err_EMUNGE_BAD_REALM => MungeError::BadRealm,
+            crate::munge_err_EMUNGE_CRED_INVALID => MungeError::CredInvalid,
+            crate::munge_err_EMUNGE_CRED_EXPIRED => MungeError::CredExpired,
+            crate::munge_err_EMUNGE_CRED_REWOUND => MungeError::CredRewound,
+            crate::munge_err_EMUNGE_CRED_REPLAYED => MungeError::CredReplayed,
+            crate::munge_err_EMUNGE_CRED_UNAUTHORIZED => MungeError::CredUnauthorized,
+            crate::munge_err_EMUNGE_SUCCESS => MungeError::Success,
+
+            _ => MungeError::BadArg,
+        }
+    }
 }
