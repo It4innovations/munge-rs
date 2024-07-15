d60cb5813d2845d7d1c3f244abc9dc1928e3768b
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-15 15:36:33 +0200

    Started work on more setters on the Context struct

--- src/ctx.rs
+++ src/ctx.rs
@@ -1,6 +1,6 @@
 use std::path::PathBuf;
 
-use crate::enums::{self, MungeError};
+use crate::enums::{self, MungeError, MungeMac, MungeOption, MungeZip};
 
 pub struct Context {
     ctx: *mut crate::munge_ctx,
@@ -16,16 +16,35 @@ impl Context {
         }
     }
 
-    pub fn set_socket(&mut self, path: PathBuf) -> MungeError {
+    /// Sets the path to the daemons socket of this [`Context`].
+    pub fn set_socket(&mut self, path: PathBuf) -> Result<(), MungeError> {
         self.socket = path;
-        let mut err = 42;
-        err = unsafe {
-            crate::munge_ctx_set(self.ctx, enums::MungeOption::SOCKET as i32, &self.socket)
-        };
-        MungeError::from_u32(err)
+        let mut _err = 42;
+        _err = unsafe { crate::munge_ctx_set(self.ctx, MungeOption::SOCKET as i32, &self.socket) };
+        if MungeError::from_u32(_err) != MungeError::Success {
+            Err(MungeError::from_u32(_err))
+        } else {
+            Ok(())
+        }
     }
 
-    // pub fn set_
+    /// Sets the message auth code type of this [`Context`]
+    pub fn set_mac_type(&self, macType: MungeMac) -> Result<(), MungeError> {
+        let mut _err = 42;
+        _err = unsafe { crate::munge_ctx_set(self.ctx, MungeOption::MAC_TYPE as i32, macType) };
+        if MungeError::from_u32(_err) != MungeError::Success {
+            Err(MungeError::from_u32(_err))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Sets the compression type of this [`Context`]
+    pub fn set_zip_type(&self, zipType: MungeZip) {
+        todo!()
+    }
+
+    //TODO: Other setters
 }
 
 impl Drop for Context {
@@ -36,15 +55,28 @@ impl Drop for Context {
 
 #[cfg(test)]
 mod contextTests {
-    use crate::{ctx::Context, enums::MungeError};
+    use crate::{ctx::Context, enums::MungeMac};
     use std::path::PathBuf;
 
     #[test]
-    fn create_munge_context() {
+    fn create_ctx_with_socket() {
         let mut ctx = Context::new();
-        let err = ctx.set_socket(PathBuf::from("/usr/local/var/run/munge/munge.socket.2"));
-        assert_eq!(err, MungeError::Success);
+        assert!(ctx
+            .set_socket(PathBuf::from("/usr/local/var/run/munge/munge.socket.2"))
+            .is_ok());
         assert!(!ctx.ctx.is_null())
+    }
+
+    #[test]
+    fn set_mac_type() {
+        let ctx = Context::new();
+        assert!(ctx.set_mac_type(MungeMac::MD5).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::None).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::SHA1).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::SHA256).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::SHA512).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::RIPEMD160).is_ok());
+        assert!(ctx.set_mac_type(MungeMac::Default).is_ok());
     }
 
     // Do we need `munge_ctx_get()`?
