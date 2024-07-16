bf54e91d0ff12adcf244917f3f218fb84e06b496
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-16 15:33:28 +0200

    Improved some Error handling

--- src/enums.rs
+++ src/enums.rs
@@ -1,4 +1,9 @@
-use std::fmt::Debug;
+use std::{
+    error::{self},
+    ffi::NulError,
+    fmt,
+    str::Utf8Error,
+};
 
 /// Represents MUNGE context options
 #[repr(u32)]
@@ -64,6 +69,62 @@ impl MungeError {
             crate::munge_err_EMUNGE_CRED_UNAUTHORIZED => MungeError::CredUnauthorized,
             // crate::munge_err_EMUNGE_SUCCESS => MungeError::Success,
             _ => MungeError::BadArg,
+        }
+    }
+}
+
+impl error::Error for MungeError {}
+
+impl fmt::Display for MungeError {
+    fn fmt(&'_ self, f: &'_ mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Error {
+    MungeError(MungeError),
+    InvalidUtf8,
+    InnerNull,
+}
+
+impl error::Error for Error {
+    fn source(&'_ self) -> Option<&'_ (dyn error::Error + 'static)> {
+        match self {
+            Error::MungeError(ref munge_error) => Some(munge_error as _),
+            _ => None,
+        }
+    }
+}
+
+impl From<MungeError> for Error {
+    fn from(munge_error: MungeError) -> Self {
+        Error::MungeError(munge_error)
+    }
+}
+impl From<Utf8Error> for Error {
+    fn from(_value: Utf8Error) -> Self {
+        Error::InvalidUtf8
+    }
+}
+impl From<NulError> for Error {
+    fn from(_value: NulError) -> Self {
+        Error::InnerNull
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&'_ self, stream: &'_ mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Error::MungeError(ref munge_err) => write!(stream, "munge errored: {}", munge_err,),
+            Error::InvalidUtf8 => write!(
+                stream,
+                "C string to Rust string lift failed: got non UTF8 output",
+            ),
+            Error::InnerNull => write!(
+                stream,
+                "Rust string to C string lift failed: input had inner null",
+            ),
         }
     }
 }
