317e816f2ae2c54a88cc93470e4de4e7f2e295af
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-16 14:04:35 +0200

    Added encode

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,7 @@ include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 pub mod credential;
 pub mod ctx;
 pub mod enums;
+pub mod munge;
 
 // C prototype: `munge_err_t munge_encode(char **cred, munge_ctx_t ctx, const void *buf, int len);`
 // pub fn encode(payload: Option<&'_ [u8]>) -> Result<String, error::MungeError> {
@@ -45,6 +46,7 @@ mod libTests {
             "{:?}",
             enums::MungeError::from_u32(_err)
         );
+        println!("Roundtrip Credential {:?}", &cred);
 
         let cred_value: *const c_char = unsafe { *cred };
 
@@ -79,8 +81,10 @@ mod libTests {
         //     panic!("Failed to create munge context!");
         // }
         let mut _err: u32 = 42;
+        let mut _set_err: u32 = 69;
+        let mut _err_desc: &str = "";
         unsafe {
-            munge_ctx_set(
+            _set_err = munge_ctx_set(
                 ctx,
                 munge_opt_MUNGE_OPT_SOCKET as i32,
                 "/usr/local/var/run/munge/munge.socket.2",
@@ -93,5 +97,6 @@ mod libTests {
             "{:?}",
             enums::MungeError::from_u32(_err)
         );
+        println!("Encode credential: {:?}", cred);
     }
 }
---
+++ src/munge.rs
@@ -0,0 +1,72 @@
+use std::{
+    ffi::{self, CStr, CString},
+    ptr,
+};
+
+use crate::{credential::Credential, ctx, enums::MungeError};
+
+//TODO: Returns encoded credential. Takes optional context and optional payload.
+//
+/// INTERNALLY:  
+/// Creates a credential contained in a NUL-terminated base64 string.
+///   A payload specified by a buffer [buf] of length [len] can be
+///   encapsulated in as well.
+/// If the munge context [ctx] is NULL, the default context will be used.
+/// A pointer to the resulting credential is returned via [cred]; the caller
+///   is responsible for freeing this memory.
+/// Returns EMUNGE_SUCCESS if the credential is successfully created;
+///   o/w, sets [cred] to NULL and returns the munge error number.
+///   If a [ctx] was specified, it may contain a more detailed error
+///   message accessible via munge_ctx_strerror().
+///
+/// `munge_err_t munge_encode (char **cred, munge_ctx_t ctx, const void *buf, int len);`
+///
+pub fn encode(msg: &str, ctx: Option<ctx::Context>) -> Result<String, MungeError> {
+    let mut cred: *mut ffi::c_char = ptr::null_mut();
+    let len: ffi::c_int = msg.len().try_into().unwrap();
+    let buf: *const ffi::c_void = CString::new(msg).unwrap().into_raw() as *const ffi::c_void;
+
+    let err: u32;
+
+    if let Some(ctx) = ctx {
+        err = unsafe { crate::munge_encode(&mut cred, ctx.context(), buf, len) };
+    } else {
+        err = unsafe { crate::munge_encode(&mut cred, ptr::null_mut(), buf, len) };
+    }
+    if err != 0 {
+        Err(MungeError::from_u32(err))
+    } else {
+        let resp = Ok(unsafe { CStr::from_ptr(cred as *const i8) }
+            .to_str()
+            .unwrap()
+            .to_string());
+        unsafe { libc::free(cred as *mut ffi::c_void) };
+        resp
+    }
+}
+
+//TODO: Returns decoded credential and any included payload.
+pub fn decode() -> Result<Credential, MungeError> {
+    todo!()
+}
+
+#[cfg(test)]
+mod munge_tests {
+    use std::path::PathBuf;
+
+    use crate::{ctx::Context, munge};
+
+    #[test]
+    fn encode_test() {
+        let cred = munge::encode("Hello World! 'aaaa'", None).expect("Failed to encode");
+        println!("{:?}", cred);
+    }
+    #[test]
+    fn encode_test_w_ctx() {
+        let mut ctx = Context::new();
+        ctx.set_socket(PathBuf::from("/usr/local/var/run/munge/munge.socket.2"))
+            .unwrap();
+        let cred = munge::encode("Hello World!", Some(ctx)).expect("Failed to encode");
+        println!("Cred with context: {:?}", cred);
+    }
+}
