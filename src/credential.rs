72de3a4c57a7e28cbc6329348044e20a2c1dd4e3
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-07-17 11:34:25 +0200

    Cleanup

--- src/credential.rs
+++ src/credential.rs
@@ -4,15 +4,3 @@ pub struct Credential {
     pub gid: u32,
     pub message: String,
 }
-
-impl Credential {
-    pub fn uid(self) -> u32 {
-        self.uid
-    }
-    pub fn gid(self) -> u32 {
-        self.gid
-    }
-    pub fn message(self) -> String {
-        self.message
-    }
-}
