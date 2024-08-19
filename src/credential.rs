29ac88b14a4f67745e4bbf91cef172afdfc773ab
Ondra-Kavan <30350031+Ondra-Kavan@users.noreply.github.com>
2024-08-19 13:05:31 +0200

    Added Derives to credentials
    Patch version bump

--- src/credential.rs
+++ src/credential.rs
@@ -2,7 +2,7 @@
 ///
 /// The `Credential` struct encapsulates the user ID, group ID, and the associated
 /// message retrieved during the encoding or decoding process.
-#[derive(Debug)]
+#[derive(Debug, Default, Clone)]
 pub struct Credential {
     /// User ID (UID) associated with the credential.
     pub uid: u32,
